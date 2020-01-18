6149a718d372c67cfd18d4740b074c3da43a38d6
Julio García Pérez <julio.garciaperez@telefonica.com>
2020-01-18 14:39:19 +0100

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 require('./config/db.config');
 const passportConfig = require('./config/passport.config')
@@ -15,6 +16,7 @@ const session = require('./config/session.config');
  * Configure express
  */
 const app = express();
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
